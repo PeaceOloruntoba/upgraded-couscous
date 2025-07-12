6158b177086b91ee5327232226cec8278a49f148
PeaceOloruntoba <profprincepeace@gmail.com>
2025-07-12 19:38:36 +0100

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,20 +1,18 @@
 import { TouchableOpacity, Text, TouchableOpacityProps } from 'react-native';
-import { useTailwind } from 'nativewind';
 
 interface ButtonProps extends TouchableOpacityProps {
   title: string;
 }
 
 export const Button = ({ title, ...props }: ButtonProps) => {
-  const { tw } = useTailwind();
   return (
     <TouchableOpacity
       {...props}
       accessibilityRole="button"
       accessibilityLabel={title}
-      style={tw`bg-blue-600 p-4 rounded-lg`}
+      className={`bg-blue-600 p-4 rounded-lg`}
     >
-      <Text style={tw`text-white text-lg font-bold text-center`}>{title}</Text>
+      <Text className={`text-white text-lg font-bold text-center`}>{title}</Text>
     </TouchableOpacity>
   );
 };
