29b14a3c0a2e1a20e6cf9d0bd6b2eb4b294f876e
PeaceOloruntoba <profprincepeace@gmail.com>
2025-07-12 19:35:38 +0100

    Update SubtitleDisplay.tsx

--- src/components/SubtitleDisplay.tsx
+++ src/components/SubtitleDisplay.tsx
@@ -1,18 +1,16 @@
 import { Text, View } from 'react-native';
 import * as Animatable from 'react-native-animatable';
-import { useTailwind } from 'nativewind';
 
 interface SubtitleDisplayProps {
   text: string;
 }
 
 export const SubtitleDisplay = ({ text }: SubtitleDisplayProps) => {
-  const { tw } = useTailwind();
   return (
-    <View style={tw`absolute bottom-10 w-full bg-black bg-opacity-50 p-4`}>
+    <View className={`absolute bottom-10 w-full bg-black bg-opacity-50 p-4`}>
       <Animatable.Text
         animation="fadeIn"
-        style={tw`text-white text-lg font-bold text-center`}
+        className={`text-white text-lg font-bold text-center`}
         accessibilityLabel="Subtitles"
       >
         {text}
