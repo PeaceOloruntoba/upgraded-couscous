b656745e732803402cdb0fb7f3ba5d5b50cdc6df
PeaceOloruntoba <profprincepeace@gmail.com>
2025-07-12 19:36:23 +0100

    Update Modal.tsx

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,5 +1,4 @@
 import { Modal as RNModal, View, Text, ModalProps } from 'react-native';
-import { useTailwind } from 'nativewind';
 import { Button } from './Button';
 
 interface ModalPropsExtended extends ModalProps {
@@ -10,14 +9,13 @@ interface ModalPropsExtended extends ModalProps {
 }
 
 export const Modal = ({ title, message, onConfirm, onCancel, ...props }: ModalPropsExtended) => {
-  const { tw } = useTailwind();
   return (
     <RNModal {...props} transparent animationType="fade">
-      <View style={tw`flex-1 justify-center items-center bg-black bg-opacity-50`}>
-        <View style={tw`bg-white p-6 rounded-lg w-3/4`}>
-          <Text style={tw`text-xl font-bold mb-4`}>{title}</Text>
-          <Text style={tw`text-lg mb-4`}>{message}</Text>
-          <View style={tw`flex-row justify-between`}>
+      <View className={`flex-1 justify-center items-center bg-black bg-opacity-50`}>
+        <View className={`bg-white p-6 rounded-lg w-3/4`}>
+          <Text className={`text-xl font-bold mb-4`}>{title}</Text>
+          <Text className={`text-lg mb-4`}>{message}</Text>
+          <View className={`flex-row justify-between`}>
             <Button title="Cancel" onPress={onCancel} />
             <Button title="Confirm" onPress={onConfirm} />
           </View>
