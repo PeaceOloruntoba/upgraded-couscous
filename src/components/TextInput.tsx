0946c67781361cefed1c00a3817713a0146d3c57
PeaceOloruntoba <profprincepeace@gmail.com>
2025-07-12 19:36:45 +0100

    Update TextInput.tsx

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,17 +1,15 @@
 import { TextInput as RNTextInput, TextInputProps } from 'react-native';
-import { useTailwind } from 'nativewind';
 
 interface InputProps extends TextInputProps {
   accessibilityLabel: string;
 }
 
 export const TextInput = ({ accessibilityLabel, ...props }: InputProps) => {
-  const { tw } = useTailwind();
   return (
     <RNTextInput
       {...props}
       accessibilityLabel={accessibilityLabel}
-      style={tw`border-2 border-gray-300 p-3 rounded-lg text-lg bg-white text-black`}
+      className={`border-2 border-gray-300 p-3 rounded-lg text-lg bg-white text-black`}
     />
   );
 };
