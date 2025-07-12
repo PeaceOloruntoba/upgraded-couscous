6723b695beadc376e2788e29a7ee184d6c851e0b
PeaceOloruntoba <profprincepeace@gmail.com>
2025-07-12 19:46:16 +0100

    Update payment.tsx

--- src/app/payment.tsx
+++ src/app/payment.tsx
@@ -1,11 +1,9 @@
 import { View, Text } from 'react-native';
-import { useTailwind } from 'nativewind';
 import { CardField } from '@stripe/stripe-react-native';
-import { Button } from '../components';
 import { usePayment } from '../hooks/payment';
+import { Button } from '@/components/Button';
 
 export default function Payment() {
-  const { tw } = useTailwind();
   const { addPaymentMethod } = usePayment();
 
   const handleAdd = async () => {
@@ -14,11 +12,11 @@ export default function Payment() {
   };
 
   return (
-    <View style={tw`flex-1 justify-center p-4 bg-gray-100`}>
-      <Text style={tw`text-2xl font-bold mb-4`}>Add Payment Method</Text>
+    <View className={`flex-1 justify-center p-4 bg-gray-100`}>
+      <Text className={`text-2xl font-bold mb-4`}>Add Payment Method</Text>
       <CardField
         accessibilityLabel="Card Details"
-        style={tw`h-12 border-2 border-gray-300 rounded-lg mb-4`}
+        className="h-12 border-2 border-gray-300 rounded-lg mb-4"
       />
       <Button title="Add Card" onPress={handleAdd} />
     </View>
