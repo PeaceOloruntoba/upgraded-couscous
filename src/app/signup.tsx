fd3a48503887239709ae01a3b8e0bd46c3d1f43a
PeaceOloruntoba <profprincepeace@gmail.com>
2025-07-12 19:43:26 +0100

    Update signup.tsx

--- src/app/signup.tsx
+++ src/app/signup.tsx
@@ -1,12 +1,11 @@
 import { View, Text } from 'react-native';
-import { useTailwind } from 'nativewind';
-import { TextInput, Button } from '../components';
 import { useAuth } from '../hooks/auth';
 import { useState } from 'react';
 import { Link } from 'expo-router';
+import { TextInput } from '@/components/TextInput';
+import { Button } from '@/components/Button';
 
 export default function Signup() {
-  const { tw } = useTailwind();
   const { signup, googleSignIn, tikTokSignIn } = useAuth();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -16,8 +15,8 @@ export default function Signup() {
   };
 
   return (
-    <View style={tw`flex-1 justify-center p-4 bg-gray-100`}>
-      <Text style={tw`text-2xl font-bold mb-4`}>Sign Up</Text>
+    <View className={`flex-1 justify-center p-4 bg-gray-100`}>
+      <Text className={`text-2xl font-bold mb-4`}>Sign Up</Text>
       <TextInput
         accessibilityLabel="Email"
         value={email}
