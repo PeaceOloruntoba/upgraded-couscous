6be375117f6e120d9bd292fc065bbf0b27767bfe
PeaceOloruntoba <profprincepeace@gmail.com>
2025-07-13 03:33:10 +0100

    Update TextInput.tsx

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -2,14 +2,15 @@ import { TextInput as RNTextInput, TextInputProps } from 'react-native';
 
 interface InputProps extends TextInputProps {
   accessibilityLabel: string;
+  className?: string;
 }
 
-export const TextInput = ({ accessibilityLabel, ...props }: InputProps) => {
+export const TextInput = ({ accessibilityLabel, className = '', ...props }: InputProps) => {
   return (
     <RNTextInput
       {...props}
       accessibilityLabel={accessibilityLabel}
-      className={`border-2 border-gray-300 p-3 rounded-lg text-lg bg-white text-black`}
+      className={`border-2 border-gray-200 p-4 rounded-xl text-lg bg-white text-black shadow-sm ${className}`}
     />
   );
 };
